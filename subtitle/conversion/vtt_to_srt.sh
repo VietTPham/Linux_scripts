ceac7e0c20b1b1d47d0b00351a42bfe8ac654005
VietTPham <pham.t.viet@gmail.com>
2016-09-06 16:08:23 -0700

    Added author and version

--- subtitle/conversion/vtt_to_srt.sh
+++ subtitle/conversion/vtt_to_srt.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+#by Viet Pham
+#Version: 2016.09.16
 
 if [ $# -lt 1 ]; then
-  echo "How to use: $0 <list of vtt file>"
+  echo "How to use: $0 <list of vtt file(s)>"
   exit 1
 fi
 for vtt in "${@:1}"; do
-  #output=$(sed 's/...$/srt/g' <<< $vtt 
   cat $vtt  | sed -e 's/  //g' -e 's/^ //g' | tail -n +3 > $(sed 's/...$/srt/g' <<< $vtt)
-  dos2unix -q $(sed 's/...$/srt/g' <<< $vtt)
+  #dos2unix -q $(sed 's/...$/srt/g' <<< $vtt) Need to review if this is needed or not
 done
